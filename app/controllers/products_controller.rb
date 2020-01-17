d81ea157e145611f0736da29e4300d2af59876d4
Mikhail Melnyk <mikhail.melnyk@nixsolutions.com>
2020-01-17 18:01:32 +0200

    Test create and update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,28 +6,14 @@ class ProductsController < AuthenticatedController
   end
 
   def new
-
-    binding.pry
-
     @product = ShopifyAPI::Product.new(set_attributes)
   end
 
   def create
-
-
-    @product = ShopifyAPI::Product.new(params)
-
-    # @product.add_metafield(ShopifyAPI::Metafield.new({
-    #   :description => 'Location',
-    #   :namespace => "inventory",
-    #   :key => "position",
-    #   :value => '123123213213213213213',
-    #   :owner_type => "PRODUCT",
-    #   :value_type => 'string'
-    # }))
+    @product = ShopifyAPI::Product.create(permit_params)
 
     if @product.save
-      redirect_to @product
+      render :show
     else
       render :new
     end
@@ -47,7 +33,12 @@ class ProductsController < AuthenticatedController
     @product.body_html = params[:shopify_api_product][:body_html]
     @product.product_type = params[:shopify_api_product][:product_type]
     @product.variants[0].price = params[:variant][:price]
-    @product.metafields[0].update_attributes(value: params[:position][:value])
+
+    if @product.metafields[0].nil?
+      add_position(@product, params[:position][:value]) 
+    else
+      @product.metafields[0].update_attributes(value: params[:position][:value])
+    end
 
     if @product.save
       render :show
@@ -70,10 +61,48 @@ class ProductsController < AuthenticatedController
 
   private 
 
+  def add_position(product, value)
+    product.add_metafield(ShopifyAPI::Metafield.new({
+      :description => 'Location',
+      :namespace => "inventory",
+      :key => "position",
+      :value => value,
+      :owner_type => "PRODUCT",
+      :value_type => 'string'
+    }))
+  end
+
   def permit_params
-    params.permit(:title, :body_html, :product_type)
+    params[:shopify_api_product]
+      .permit(:title, :body_html, :product_type)
+      .merge({ vendor: "nixshoptest" })
   end
 end
 
 
+# Create metafieldStorefrontVisibility
+#
+# client = ShopifyAPI::GraphQL.new
 
+# SHOP_NAME_QUERY = client.parse <<-'GRAPHQL'
+# mutation {
+#   metafieldStorefrontVisibilityCreate(
+#   input: {
+#     namespace: "test",
+#     key: "test",
+#     ownerType: PRODUCT
+#   }
+#   ) {
+#     metafieldStorefrontVisibility {
+#       id
+#     }
+#     userErrors {
+#       field
+#       message
+#     }
+#   }
+# }
+# GRAPHQL
+
+# result = client.query(SHOP_NAME_QUERY)
+# result.data.shop.name
